bdf3e12dc369e33ac9c5b759d0e9b58c3f2c3e34
Larry Luk <larryluk526@gmail.com>
2017-12-07 11:45:53 -0500

    animations done

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navBarHeight: NSLayoutConstraint!
+    @IBOutlet weak var navBarView: UIView!
+    @IBOutlet weak var addBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func addBtnPressed(_ sender: UIButton) {
+        if (navBarHeight.constant == 64) {
+            UIView.animate(withDuration: 0.7,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut,
+                           animations: {
+                            self.navBarHeight.constant = 200
+                            self.view.layoutIfNeeded()
+                            self.addBtn.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+            },
+                           completion: nil)
+            
+        } else {
+            UIView.animate(withDuration: 0.7,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut,
+                           animations: {
+                            self.navBarHeight.constant = 64
+                            self.view.layoutIfNeeded()
+                            self.addBtn.transform = CGAffineTransform.identity
+            },
+                           completion: nil)
+        }
+      }
 }
 
